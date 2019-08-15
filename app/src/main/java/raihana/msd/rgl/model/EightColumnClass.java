cab091fcb6e9d1390f1d632a340ec42a3af63951
sapta <msaptadw@yahoo.com>
2019-08-15 09:59:44 +0700

    disc, price can be edit

---
+++ app/src/main/java/raihana/msd/rgl/model/EightColumnClass.java
@@ -0,0 +1,52 @@
+package raihana.msd.rgl.model;
+
+public class EightColumnClass {
+
+    //declaration
+    private String Column_1, Column_2,Column_3,Column_4,Column_5,Column_6,Column_7,Column_8;
+
+    //constructor
+    public EightColumnClass() {
+    }
+
+    //constructor
+    public EightColumnClass(String Column_1, String Column_2, String Column_3, String Column_4,
+                            String Column_5, String Column_6, String Column_7, String Column_8) {
+        this.Column_1 = Column_1;
+        this.Column_2 = Column_2;
+        this.Column_3 = Column_3;
+        this.Column_4 = Column_4;
+        this.Column_5 = Column_5;
+        this.Column_6 = Column_6;
+        this.Column_7 = Column_7;
+        this.Column_8 = Column_8;
+    }
+
+    //getter
+    public String getColumn_1() {
+        return Column_1;
+    }
+    public String getColumn_2() {
+        return Column_2;
+    }
+    public String getColumn_3() {
+        return Column_3;
+    }
+    public String getColumn_4() {
+        return Column_4;
+    }
+    public String getColumn_5() {
+        return Column_5;
+    }
+    public String getColumn_6() {
+        return Column_6;
+    }
+    public String getColumn_7() {
+        return Column_7;
+    }
+    public String getColumn_8() {
+        return Column_8;
+    }
+
+    //setter
+}
